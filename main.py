0d43e8fab5c731d647ae6a1394583bfece86d520
sandrowiedmer <wiedmer.sandro@gmail.com>
2025-03-25 21:41:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,5 @@
 
 import streamlit as st
 
-st.markdown("# Text Simplification App")
-st.markdown("In the 2nd tab you can find a demonstration of text simplification using an LLM (FLAN-T5-Small)")
+st.markdown("# Text Simplification")
+st.markdown("In the tab 'text simplification app' you can find a demonstration of text simplification using an LLM (FLAN-T5-Small), and in 'description' some words about the FLAN-T5-Small model and text simplification.")
